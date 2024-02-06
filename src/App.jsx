60ccf2b9fb8b8474abba8a670b3a07640f0a85f1
z9877969 <z9877969@ukr.net>
2024-02-06 17:45:36 +0200

    adds code spliting to reactdatepicker locale

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,57 @@ import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Loader from "./components/Loader/Loader";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
-import { uk } from "date-fns/locale";
-import "react-datepicker/dist/react-datepicker.css";
 
 const HomePage = lazy(() => import("./pages/HomePage.jsx"));
 const PricesPage = lazy(() => import("./pages/PricesPage.jsx"));
 const FormPage = lazy(() => import("./pages/FormPage.jsx"));
 const OrderPage = lazy(() => import("./pages/OrderPage.jsx"));
+const BaseSettings = lazy(() => import("./components/BaseSettings.jsx"));
 
 function App() {
-  registerLocale("uk", uk);
-  setDefaultLocale("uk");
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route
-          index
-          element={
-            <Suspense fallback={<Loader />}>
-              <HomePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="prices"
-          element={
-            <Suspense fallback={<Loader />}>
-              <PricesPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="form"
-          element={
-            <Suspense fallback={<Loader />}>
-              <FormPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="order/:orderStage"
-          element={
-            <Suspense fallback={<Loader />}>
-              <OrderPage />
-            </Suspense>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
+    <>
+      <Suspense fallback={null}>
+        <BaseSettings />
+      </Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<Loader />}>
+                <HomePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="prices"
+            element={
+              <Suspense fallback={<Loader />}>
+                <PricesPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="form"
+            element={
+              <Suspense fallback={<Loader />}>
+                <FormPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="order/:orderStage"
+            element={
+              <Suspense fallback={<Loader />}>
+                <OrderPage />
+              </Suspense>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
