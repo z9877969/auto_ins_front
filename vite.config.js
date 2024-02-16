00af11730954126e5c5416a9fb4a4022c6cc5b1e
z9877969 <z9877969@ukr.net>
2024-02-16 12:49:30 +0200

    adds pormone payee id to env

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 import dotenv from "dotenv";
@@ -6,7 +6,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: "/",
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
